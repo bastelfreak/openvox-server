2f28651ef5008f242e85e7520d54daad3b542a0d
nmburgan <13688219+nmburgan@users.noreply.github.com>
2026-01-03 14:17:45 -0800

    Bump Xmx to 4G and increase length of test timeout
    These tests seem to take a very long time on GitHub runners, so this extends the timeout so they aren't killed prematurely. Also, we're seeing OOMs on some of the multithreaded cells, so this increases the heap size during testing to 4GB.

--- project.clj
+++ project.clj
@@ -145,7 +145,7 @@
                     ;; use core.async and need more than eight threads to run
                     ;; properly; this setting overrides the default value.  Without
                     ;; it the metrics tests will hang.
-                    :jvm-opts ["-Dclojure.core.async.pool-size=50"]
+                    :jvm-opts ["-Dclojure.core.async.pool-size=50", "-Xms4g", "-Xmx4g"]
                     ;; Use humane test output so you can actually see what the problem is
                     ;; when a test fails.
                     :dependencies [[pjstadig/humane-test-output "0.11.0"]]
